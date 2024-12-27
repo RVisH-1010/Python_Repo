e324ff8067d8f759174d592d8f703629203aacdb
Rohan Vishwakarama <rohanvish4you@gmail.com>
2024-12-27 08:48:18 +0000

    inheritance in py

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-# print("Hello World")
-class person:
-    def __init__(self,n , o):
-        print("class person is called.")
-        self.name = n
-        self.occ = o
+# # print("Hello World")
+# class person:
+#     def __init__(self,n , o):
+#         print("class person is called.")
+#         self.name = n
+#         self.occ = o
 
-    name = "harry"
-    occ = "coder"
-    def show(self):
-        print(f"my name is {name} & i am {occ}") 
+#     name = "harry"
+#     occ = "coder"
+#     def show(self):
+#         print(f"my name is {name} & i am {occ}") 
 
-a = person("rohan ","student")
-a.show()
+# a = person("rohan ","student")
+# a.show()
 
+
+
+# leaving 3 lines for a new topics
+# 1.inheritance 
+
+# class employee:
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id = id
+#     name ="abc"
+#     id = "230"
+
+#     def show(self):
+#         print(f"{self.name} is a employee with id: {self.id}")
+
+# class programmer(employee):
+#     def showlang(self):
+#         print("the language used is python")
+
+# a = programmer("Harry_potter", 7989)
+# a.showlang()
+# a.show()
+
+        
+
+# 2. access modifier
