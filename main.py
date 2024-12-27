cbb79e617027f8725d3be9685dca5e449ae45c77
Rohan Vishwakarama <rohanvish4you@gmail.com>
2024-12-27 08:29:26 +0000

    oops and html

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Hello World")
+# print("Hello World")
+class person:
+    def __init__(self,n , o):
+        print("class person is called.")
+        self.name = n
+        self.occ = o
+
+    name = "harry"
+    occ = "coder"
+    def show(self):
+        print(f"my name is {name} & i am {occ}") 
+
+a = person("rohan ","student")
+a.show()
 
