c8104bf5ca48cee90264380614192fd99d72fede
Rohan Vishwakarama <rohanvish4you@gmail.com>
2024-12-30 06:43:09 +0000

    all done till ovrldng

--- Day_4.py
+++ Day_4.py
@@ -14,24 +14,49 @@ print("starting of day 4!!!")
 # print(hari.name,hari.id, hari.lang)        
 # print(rohan.name, rohan.id)
 
+
+
 # 2. magic or dunder method in py
-class employee:
-    def __init__(self,name):
-        self.name = name
-    def __len__(self):
-        i=0 
-        for c in self.name:
-            i = i+1
-        return i
-    # def __str__(self):
-    #     return f"the employee is {self.name} (str)"
+# class employee:
+#     def __init__(self,name):
+#         self.name = name
+#     def __len__(self):
+#         i=0 
+#         for c in self.name:
+#             i = i+1
+#         return i
+#     # def __str__(self):
+#     #     return f"the employee is {self.name} (str)"
     
-    def __repr__(self):
-        return f"the empployee is {self.name} (repr)"
-    def __call__(self, *args, **kwds):
-        print("hello world!!")
+#     def __repr__(self):
+#         return f"the empployee is {self.name} (repr)"
+#     def __call__(self, *args, **kwds):
+#         print("hello world!!")
+
+# e = employee("Roahn")
+# print(e.name)
+# print(len(e))
 
 
-e = employee("Roahn")
-print(e.name)
-print(len(e))
+
+# 3. method overriding
+# class shape:
+#     def __init__(self,x, y):
+#         self.x = x
+#         self.y = y
+#     def area(self):
+#         return self.x*self.y    
+
+# class circle(shape):
+#     def __init__(self, radius):
+#         self.radius = radius
+#         super().__init__(radius,radius)
+#     def area(self):
+#         return 3.14*super().area()        
+
+# c = circle(7)
+# print(c.area())            
+
+
+
+# operator overloading
